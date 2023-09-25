f2730d14e42155f8c57572a14050a70fd36ca27e
“hiranwj” <hiranwj.connect@gmail.com>
2023-09-25 15:00:17 +0530

    Initial commit

---
+++ src/test/java/io/xdoto/energyusagedata/EnergyUsageDataApplicationTests.java
@@ -0,0 +1,13 @@
+package io.xdoto.energyusagedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyUsageDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
